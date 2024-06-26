766634c101fb5af1f0d6d70fda3dad941f01e141
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-06-26 15:22:55 +0600

    send urlPrefix to client

--- routes/secret.js
+++ routes/secret.js
@@ -2,15 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+const generatePrefix = () => {
+    const random64BitHexCode = Array.from({ length: 8 }, () => Math.floor(Math.random() * 16).toString(16)).join('');
+    return random64BitHexCode;
+};
+
 router.post('/', (req, res) => {
     const clientSecret = req.body;
+    console.log(clientSecret);
     const serverSecret = process.env.LOGIN_SECRET;
 
-    if (clientSecret !== serverSecret) {
-        return res.status(401).send({ message: 'Unauthorized Access!' });
+    if (clientSecret.code !== serverSecret) {
+        return res.status(422).send({ message: 'Invalid Secret Code!' });
     }
 
-    res.status(200).send({ message: 'Secret Matched!' });
+    res.status(200).send({ message: 'Secret Matched!', urlPrefix: generatePrefix() });
 });
 
 export default router;
