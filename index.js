20d8265bea0cd22020d4862822af3f659daa4637
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-27 18:24:30 +0600

    added error handler middlewares

--- index.js
+++ index.js
@@ -51,6 +51,21 @@ app.get("/", async (req, res) => {
     res.send("Portfolio Server is Running!");
 });
 
+// error handler for 404
+app.use((req, res, next) => {
+    const error = new Error("Requested URL Not Found!");
+    error.status = 404;
+    next(error);
+});
+
+// final error handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(error.status || 500).send({
+        message: error.message || "Internal Server Error!",
+    });
+});
+
 const run = async () => {
     await connectDB();
 
