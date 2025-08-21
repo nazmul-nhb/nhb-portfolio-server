b556cbf474aa21171c8b893137f83f8e639842b4
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-21 13:22:51 +0600

    update(cors): updated cors config with latest domain

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ dotenv.config();
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://nazmul-nhb.dev',
+    'https://www.nazmul-nhb.dev',
     'https://nazmul-nhb.web.app',
     'https://nazmul-nhb.vercel.app',
     'https://nazmul-nhb.firebaseapp.com',
